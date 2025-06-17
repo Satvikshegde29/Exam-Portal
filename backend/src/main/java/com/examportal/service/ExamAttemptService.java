6a7f4950158ce0dfcff0c14fe6a05cb85f07a8e5
Satvikshegde29 <Satvik.Hegde@cognizant.com>
2025-06-17 14:36:12 +0530

    removed the unnescessary files

--- backend/src/main/java/com/examportal/service/ExamAttemptService.java
+++
@@ -1,9 +0,0 @@
-package com.examportal.service;
-
-import com.examportal.model.ExamAttempt;
-import java.util.Map;
-
-public interface ExamAttemptService {
-    ExamAttempt submitAttempt(Long userId, Long examId, Map<Long, Integer> answers);
-    ExamAttempt getReport(Long userId, Long examId);
-}
